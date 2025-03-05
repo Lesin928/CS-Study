a4b1526ff74ab780d2cac71edb7e1a8b996b1908
Lesin <43850042+Lesin928@users.noreply.github.com>
2025-03-05 16:45:29 +0900

    2025-03-04 10250번 문제

---
+++ LSM/LSM/10250.cs
@@ -0,0 +1,17 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        int T = Convert.ToInt32(Console.ReadLine()!); 
+        for (int i = 0; i < T; i++)
+        {
+            string[] input = (Console.ReadLine()!).Split();
+            int H = int.Parse(input[0]);
+            int W = int.Parse(input[1]);
+            int N = int.Parse(input[2]);
+
+            int result = ((N - 1) / H) + ((N - 1) % H) * 100 + 101;
+            Console.WriteLine(result);
+        }
+    }
+}
