c116c0b2e919ab2f787df681db39287f7399b323
Lesin <43850042+Lesin928@users.noreply.github.com>
2025-03-05 17:52:09 +0900

    2025-03-05 1712번 문제

---
+++ LSM/LSM/1712.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Linq;
+
+namespace LSM
+{
+    class _1712
+    {
+        static void Main(string[] args)
+        {
+            int[] values = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+            int fixedCost = values[0];  
+            int variableCost = values[1];  
+            int price = values[2]; 
+
+            if (price <= variableCost)
+                Console.WriteLine("-1");  
+            else
+            {
+                int breakEvenPoint = fixedCost / (price - variableCost) + 1;
+                Console.WriteLine(breakEvenPoint); 
+            }
+        }
+    }
+}
