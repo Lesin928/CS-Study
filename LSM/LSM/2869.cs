8ff4bce8861f5524646aed1e13861755e548246a
Lesin <43850042+Lesin928@users.noreply.github.com>
2025-03-07 09:03:32 +0900

    2025-03-06 2869번 문제

---
+++ LSM/LSM/2869.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LSM
+{
+    class _2869
+    {
+        static void Main(string[] args)
+        { 
+            string[] inputs = Console.ReadLine().Split();
+            long A = long.Parse(inputs[0]);
+            long B = long.Parse(inputs[1]);
+            long V = long.Parse(inputs[2]);
+
+            if (V == A)
+            {
+                Console.WriteLine(1);
+            }
+            else if ((V - A) % (A - B) > 0)
+            {
+                Console.WriteLine((V - A) / (A - B) + 2);
+            }
+            else
+            {
+                Console.WriteLine((V - A) / (A - B) + 1);
+            }
+        }
+    }
+}
+
