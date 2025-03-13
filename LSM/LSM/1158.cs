15e7c7259691575a85428dc58def1d5d6736bcef
Lesin <43850042+Lesin928@users.noreply.github.com>
2025-03-13 19:31:49 +0900

    커밋을안햇네

---
+++ LSM/LSM/1158.cs
@@ -0,0 +1,122 @@
+﻿using System;
+
+namespace LSM
+{
+    public class Node 
+    {
+        public int Data;   
+        public Node Next;  
+
+        public Node(int data)
+        {
+            Data = data;
+            Next = null;
+        }
+    }
+
+
+    public class CLinkedList
+    {
+        public Node tail;
+
+        public bool IsEmpty()
+        {
+            return tail == null;
+        }
+
+        public void AddNode(int data)
+        {
+            Node newNode = new Node(data);
+
+            if (IsEmpty())
+            {
+                tail = newNode;
+                tail.Next = tail;
+            }
+            else
+            {
+                newNode.Next = tail.Next;  
+                tail.Next = newNode; 
+                tail = newNode;
+            }
+        }
+
+        public int RemoveNode(ref Node current, int k)
+        {
+            if (k == 1)  
+            {
+                Node toDelete = current;
+                int removedData = toDelete.Data;
+                 
+                if (current.Next == current)
+                {
+                    tail = null;
+                    current = null;
+                }
+                else
+                {
+                    tail.Next = current.Next;  
+                    current = current.Next;   
+                }
+
+                return removedData;
+            }
+             
+            for (int i = 1; i < k - 1; i++)
+            {
+                current = current.Next;
+            }
+
+            Node toDeleteK = current.Next;
+            int rData = toDeleteK.Data;
+
+            current.Next = toDeleteK.Next;
+
+            if (toDeleteK == tail)
+            {
+                tail = current;
+            }
+
+            current = current.Next;
+
+            return rData;
+        }
+
+    }
+
+    class _1158
+    {
+        static void PrintOutput(int n, int k)
+        {
+            CLinkedList list = new CLinkedList();
+            List<int> result = new List<int>();
+             
+            for (int i = 1; i <= n; i++)
+            {
+                list.AddNode(i);
+            }
+
+            Node current = list.tail.Next;  
+             
+            for (int i = 0; i < n; i++)
+            {
+                int removed = list.RemoveNode(ref current, k);
+                result.Add(removed);
+            }
+             
+            Console.WriteLine($"<{string.Join(", ", result)}>");
+        }
+
+        static void Main(string[] args)
+        {
+            string[] words = Console.ReadLine().Split(" ");
+            int N = Convert.ToInt32(words[0]);
+            int K = Convert.ToInt32(words[1]);
+
+            PrintOutput(N, K);
+
+        }
+    }
+}
+
+
---
+++ LSM/LSM/1991.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static Dictionary<char, (char left, char right)> tree = new Dictionary<char, (char left, char right)>();
+
+    // 전위 순회
+    static void PreOrder(char node)
+    {
+        if (node == '.') 
+            return;  
+        Console.Write(node);
+        PreOrder(tree[node].left);
+        PreOrder(tree[node].right);
+    }
+
+    // 중위 순회
+    static void InOrder(char node)
+    {
+        if (node == '.') 
+            return;  
+        InOrder(tree[node].left);
+        Console.Write(node);
+        InOrder(tree[node].right);
+    }
+
+    // 후위 순회
+    static void PostOrder(char node)
+    {
+        if (node == '.') 
+            return;  
+        PostOrder(tree[node].left);
+        PostOrder(tree[node].right);
+        Console.Write(node);
+    }
+
+    static void Main(string[] args)
+    {
+        int n = int.Parse(Console.ReadLine());  
+        for (int i = 0; i < n; i++)
+        {
+            string input = Console.ReadLine();
+            char parent = input[0];
+            char left = input[2];
+            char right = input[4];
+
+            tree[parent] = (left, right);
+        } 
+
+        PreOrder('A');
+        Console.WriteLine();
+        InOrder('A');
+        Console.WriteLine();
+        PostOrder('A');
+    }
+}
